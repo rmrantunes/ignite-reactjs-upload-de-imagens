f550375ed821a4fd7b94094ea2743a3ca1f4c1a1
Rafael Antunes <rafaelmessias710@gmail.com>
2021-08-15 10:42:15 -0400

    feat: get Images and add pagination logic in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,26 +18,41 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = null }) => {
+      const response = await api.get('/api/images', {
+        params: {
+          after: pageParam,
+        },
+      });
+
+      return response.data;
+    },
+    {
+      getNextPageParam: lastPage => lastPage.after,
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.map(page => page.data).flat();
   }, [data]);
-
-  // TODO RENDER LOADING SCREEN
-
-  // TODO RENDER ERROR SCREEN
-
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
   return (
     <>
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            type="button"
+            disabled={isFetchingNextPage}
+            onClick={() => fetchNextPage()}
+            mt="10"
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
